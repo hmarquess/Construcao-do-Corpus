a05ee5f7c3a5d0599d7c4d4a3a031a52067d77f6
hmarquess <102669125+hmarquess@users.noreply.github.com>
2022-10-13 08:46:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from bs4 import BeautifulSoup
-import requests
+nlp=spacy.load("en_core_web_sm")
+url = ["https://en.wikipedia.org/wiki/Natural_language_processing", "https://www.gyansetu.in/what-is-natural-language-processing/", "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8120048/", "https://languagelog.ldc.upenn.edu/nll/?p=2946", "https://www.fcg-net.org/"]
 
-url = "https://en.wikipedia.org/wiki/Genghis_Khan"
+for x in url:
+  result = requests.get(x).text
+  soup = BeautifulSoup(result, 'lxml')
 
-result = requests.get(url).text
-soup = BeautifulSoup(result, 'lxml')
+  text = "".join([i.text for i in soup.find_all('p')])
 
-frases = [i.text for i in soup.find_all('p')]
-print(frases)
+  doc = nlp(text)
+
+  for sent in doc.sents:
+    print(sent)
+
+
+
